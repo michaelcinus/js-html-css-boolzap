d1f80a37471f3ebe56b1c0f64b217c23b82bb982
Michael Cinus <michaelcinus@gmail.com>
2020-07-13 15:26:37 +0200

    JS: prova function tasto invio

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    function pressEnterLogin(event){
+        var keyCode = event.keyCode ? event.keyCode : event.which ? event.which : event.charCode;
+        if (keyCode === 13){
+          console.log("weeee");
+        }
+    };
+    
+ //var messaggio = document.getElementById("msg").value;
+
+
+
+
+
+
+
+
+
+});
+
