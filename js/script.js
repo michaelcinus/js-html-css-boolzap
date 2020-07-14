3648cb688a96897ac253a9414509badc0d64301d
Michael Cinus <michaelcinus@gmail.com>
2020-07-14 12:07:13 +0200

    correzione parziale e creazione function JS

--- js/script.js
+++ js/script.js
@@ -1,21 +1,48 @@
-$(document).ready(function(){
+$(document).ready(init(){
 
-    function pressEnterLogin(event){
-        var keyCode = event.keyCode ? event.keyCode : event.which ? event.which : event.charCode;
-        if (keyCode === 13){
-          console.log("weeee");
-        }
-    };
-    
- //var messaggio = document.getElementById("msg").value;
-
-
-
-
-
-
-
-
-
+  
 });
 
+function init() {
+
+  addSentListener();
+}
+
+
+function addSentListener{
+
+ var target = $("#new-message-input");
+ target.keyup(endKeyup);
+}
+
+function sendKeyup() {
+  
+  var key = event.which;
+  
+  if (key === 13) {
+    var input = $(this);
+    var txt = input.val();
+
+    input.val(" ");
+
+    function sendMessage (txt);
+    
+  }
+}
+
+function sendMessage (txt) {
+  var template = $("#template-message-sent > div").clone();
+  var target = $("#right-messages");
+
+  template.find("#message-text").text(txt);
+  template.find("#message-time").text(getActualHour());
+
+
+  target.append(template);
+}
+
+function getActualHour(){
+
+  var date = new date();
+  return date.getHours() + ":" + date.getMinutes;
+}
