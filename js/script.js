78dbed0a836bb34fc58fa1da3dd73d3c7a913353
Michael Cinus <michaelcinus@gmail.com>
2020-07-15 11:10:48 +0200

    JS: correzzione e creazione function ricerca chat

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+//RICERCA CHAT
+function ricercaChat () {
+
+  var target = $("#ricerca-chat");
+  target.keyup(serchKeyup);
+ }
+
+ function serchKeyup(event) {
+  
+  var input = $(this);
+  var txt = input.val();
+
+  var contacts = $(".contatti .contatto")
+  contacts.each( function(){
+
+    var contact = $(this);
+    var nomeContatto = contact.find(".nome").text();
+
+    if (nomeContatto.toLowerCase().includes(txt.toLowerCase))
+     {
+      contact.show();
+    }else {
+      contact.hide();
+    }
+
+  });
+
+} 
+
+
+
+//inviat funzione 
 function addSentListener () {
 
   var target = $("#msg");
@@ -13,42 +45,56 @@ function addSentListener () {
   if (key === 13  && txt.length > 0) {
     input.val(" ");
     sendMessage(txt);
-    window.setTimeout("receivedMessage()", 1000);
+    setTimeout("receivedMessage()", 1000);
   }
 
 }
 
+$(document).on("click", ".message-options", function(){
+
+  console.log("hai click sul messaggio");
+  
+
+});
+
+
+//INVIO MESSAGGIO
 function sendMessage(txt) {
-  console.log(txt);
   
   var template = $("#template-message-sent > div").clone();
-  var target = $(".right-messages");
+  var target = $("#right-messages");
 
   template.find(".message-text").text(txt);
-  //template.find(".message-time").text(getActualHour());
+  template.find(".message-time").text(getActualHour());
 
   target.append(template);
 
 }
 
+
+//RICEVI MESSAGGIO
 function receivedMessage() {
   
   var template = $("#template-message-received > div").clone();
-  var target = $(".right-messages");
+  var target = $("#right-messages");
 
   template.find(".message-text").text("OK");
-  //template.find(".message-time").text(getActualHour());
+  template.find(".message-time").text(getActualHour());
 
   target.append(template);
 
 }
 
+
+//GENERA ORARIO
 function getActualHour(){
 
   var date = new Date();
-  return date.getHours() + ":" + date.getMinutes;
+  return date.getHours() + ":" + date.getMinutes();
 }
 
+
+//INIT
 function init() {
 
   addSentListener();
